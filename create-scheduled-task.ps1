5202f555142d2b446cbd95db0482f1525c12b5d4
Sumeet Singh Mankoo <sumeet.mankoo@gmail.com>
2019-06-01 09:44:14 -0400

    Start the task immediately if start time missed

--- create-scheduled-task.ps1
+++ create-scheduled-task.ps1
@@ -3,8 +3,9 @@ $argument='-ExecutionPolicy ByPass -NoProfile -WindowStyle Hidden -command "& ''
 
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument $argument
 $trigger = New-ScheduledTaskTrigger -Daily -At 12:21am -DaysInterval 1
-$task = Register-ScheduledTask -TaskName "Backup Google Drive to OneDrive" -Trigger $trigger -Action $action -RunLevel Highest 
-# $task.Triggers.Repetition.Duration = "P1D" # Repeat for a duration of one day
+$settings = New-ScheduledTaskSettingsSet -StartWhenAvailable
+$task = Register-ScheduledTask -TaskName "Backup Google Drive to OneDrive" -Trigger $trigger -Action $action -RunLevel Highest -Settings $settings
+$task.Triggers.Repetition.Duration = "P1D" # Repeat for a duration of one day
 $task.Triggers.Repetition.Interval = "PT1H" # Repeat every hour, use PT30M for every 30 minutes
 $task | Set-ScheduledTask
 
